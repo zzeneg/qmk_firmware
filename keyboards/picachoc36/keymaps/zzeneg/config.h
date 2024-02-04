5b1010557d7d7b7305909b40cdef5bff2ae1b89b
Evgenii Vilkov <zzeneg@gmail.com>
2024-02-05 00:26:22 +0100

    add picachoc36

---
+++ keyboards/picachoc36/keymaps/zzeneg/config.h
@@ -0,0 +1,17 @@
+// Copyright 2022 zzeneg (@zzeneg)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 180
+#define TAPPING_TERM_PER_KEY
+
+#define CAPS_WORD_IDLE_TIMEOUT 2500
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_DEFAULT_VAL 0
+
+#define TAP_CODE_DELAY 25
